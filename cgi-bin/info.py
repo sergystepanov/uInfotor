db4263a47dac82fe192b22d06b69e1b37414b3d0
sergystepanov <sergystepanov@gmail.com>
2022-03-13 15:01:35 +0300

    Reformat code

--- cgi-bin/info.py
+++ cgi-bin/info.py
@@ -2,40 +2,43 @@
 # -*- coding: utf-8 -*-
 
 import cgi
-import sqlite3
-import re
 import codecs
-import zlib
+import sqlite3
 import sys
-sys.stdout = codecs.getwriter("utf-8")(sys.stdout.detach())
+import zlib
+
 import modbbcode
+
+sys.stdout = codecs.getwriter("utf-8")(sys.stdout.detach())
 
 ''''''
 
 
 def calc(bsize=0):
-    it=0
-    razmer=['б','Кб','Мб','Гб','Тб']
-    while bsize>1024:
-        bsize/=1024
-        it+=1
-    if it>0:
-        p=str(bsize).split(".")
-        return ('{} '+razmer[it]).format(round(bsize,2))
+    it = 0
+    razmer = ['б', 'Кб', 'Мб', 'Гб', 'Тб']
+    while bsize > 1024:
+        bsize /= 1024
+        it += 1
+    if it > 0:
+        p = str(bsize).split(".")
+        return ('{} ' + razmer[it]).format(round(bsize, 2))
     else:
-        return ('{} '+razmer[0]).format(int(bsize))
+        return ('{} ' + razmer[0]).format(int(bsize))
+
 
 def sepp(bsize=0):
     s = str(bsize)
-    return s[-12:-9] + ' ' + s[-9:-6] + ' ' + s[-6:-3] + ' ' +s[-3:]
+    return s[-12:-9] + ' ' + s[-9:-6] + ' ' + s[-6:-3] + ' ' + s[-3:]
+
 
 def sel_content(id_tor):
-    DB=sqlite3.connect('DB/content.db3')
-    cur=DB.cursor()
+    DB = sqlite3.connect('DB/content.db3')
+    cur = DB.cursor()
     cur.execute('SELECT cont FROM contents WHERE tid=?;', (id_tor,))
-    r=cur.fetchone()
+    r = cur.fetchone()
     if r:
-        S=zlib.decompress(r[0])
+        S = zlib.decompress(r[0])
         result = modbbcode.bbcode2html(S.decode('utf-8'))
     else:
         result = '''<h3 style="text-align:center">Запись отсутствует</h3>'''
@@ -43,45 +46,46 @@ def sel_content(id_tor):
     DB.close()
     return result
 
+
 def exist_table():
-    DB=sqlite3.connect('DB/content.db3')
-    cur=DB.cursor()
+    DB = sqlite3.connect('DB/content.db3')
+    cur = DB.cursor()
     cur.execute("SELECT count(*) FROM sqlite_master WHERE name='files';")
-    r=cur.fetchone()
+    r = cur.fetchone()
     cur.close()
     DB.close()
-    if r[0]==1:
+    if r[0] == 1:
         return "block"
     else:
         return "none"
 
+
 form = cgi.FieldStorage()
-tid=0
+tid = 0
 
-if "tid" in form.keys(): tid=int(form.getfirst("tid"))
+if "tid" in form.keys(): tid = int(form.getfirst("tid"))
 
-DB=sqlite3.connect('DB/torrents.db3')
-cur=DB.cursor()
+DB = sqlite3.connect('DB/torrents.db3')
+cur = DB.cursor()
 cur.execute('''SELECT name_category, name_forum, hash_info, title, size_b, date_reg
     FROM (torrent AS t inner join forum AS p on t.forum_id=p.code_forum) inner join category AS r on p.category_id=r.code_category
     WHERE file_id=%s;''' % tid)
-r=cur.fetchone()
+r = cur.fetchone()
 cur.close()
 DB.close()
 
-
-if exist_table()=='block':
+if exist_table() == 'block':
     global fl
-    fl=[] 
-    DB=sqlite3.connect('DB/content.db3')
-    cur=DB.cursor()
-    try:       
+    fl = []
+    DB = sqlite3.connect('DB/content.db3')
+    cur = DB.cursor()
+    try:
         cur.execute('SELECT files FROM files WHERE tid=%s;' % tid)
-        rr=cur.fetchone()
+        rr = cur.fetchone()
         if rr:
-            list_files=zlib.decompress(rr[0])
+            list_files = zlib.decompress(rr[0])
             fl = list_files.decode('utf-8')
-            fl= eval(fl)
+            fl = eval(fl)
         else:
             result = '''<h3 style="text-align:center">Запись отсутствует</h3>'''
     except sqlite3.OperationalError:
@@ -89,9 +93,7 @@ if exist_table()=='block':
     cur.close()
     DB.close()
 
-
-
-html_head='''<!DOCTYPE HTML>
+html_head = '''<!DOCTYPE HTML>
 <html land="ru"><head>
 <meta http-equiv="content-type" content="text/html; charset=utf-8">
 <meta name="author" content="y3401">
@@ -107,39 +109,52 @@ html_head='''<!DOCTYPE HTML>
 <div class="layer6">
 '''
 
-
-
 print('content­type: text/html\n')
 if r:
     print(html_head % (r[3]))
     print('<table class="subtable"><tr>')
-    print('<td style="vertical-align:top;"><p class="info">{}<br />&rArr; {}</p></td>'.format(r[0],r[1]))
-    print('<td style="width: 18%; vertical-align:middle"><p class="info" title="{} байт">Размер: <b>{}</b></p></td>'.format(sepp(r[4]),calc(r[4])))
-    print('<td style="width: 10%; vertical-align:middle" align="right"><div class="folder" style="display:{}"><div class="files"><h2 style="margin-bottom:3px">Список файлов:</h2>'.format(exist_table()))
+    print('<td style="vertical-align:top;"><p class="info">{}<br />&rArr; {}</p></td>'.format(r[0], r[1]))
+    print(
+        '<td style="width: 18%; vertical-align:middle"><p class="info" title="{} байт">Размер: <b>{}</b></p></td>'.format(
+            sepp(r[4]), calc(r[4])))
+    print(
+        '<td style="width: 10%; vertical-align:middle" align="right"><div class="folder" style="display:{}"><div class="files"><h2 style="margin-bottom:3px">Список файлов:</h2>'.format(
+            exist_table()))
     print('''<table class="subtable">''')
-    if len(fl)>0:
-        offset=1
-        
-        if int(fl[0][2])==0:
-            print('''<tr><td colspan=2 style="background-color:#bad5cc; padding-left: {}em"><img class="td-folder" src="../IMG/opened-folder.png"><h4 style="display:inline">{}</h4></td></tr>'''.format(offset,fl[0][1]))
-            cat="f"
+    if len(fl) > 0:
+        offset = 1
+
+        if int(fl[0][2]) == 0:
+            print(
+                '''<tr><td colspan=2 style="background-color:#bad5cc; padding-left: {}em"><img class="td-folder" src="../IMG/opened-folder.png"><h4 style="display:inline">{}</h4></td></tr>'''.format(
+                    offset, fl[0][1]))
+            cat = "f"
         else:
-            print('''<tr><td style="padding-left: {}em"><img class="td-file" src="../IMG/file.png">{}</td><td align="right" style="padding-right: 1em;width: 7em;" title="{} байт">{}</td></tr>'''.format(offset+1,fl[0][1],sepp(fl[0][2]),calc(int(fl[0][2]))))
-            cat="d"
-            offset +=1
+            print(
+                '''<tr><td style="padding-left: {}em"><img class="td-file" src="../IMG/file.png">{}</td><td align="right" style="padding-right: 1em;width: 7em;" title="{} байт">{}</td></tr>'''.format(
+                    offset + 1, fl[0][1], sepp(fl[0][2]), calc(int(fl[0][2]))))
+            cat = "d"
+            offset += 1
         for item in fl[1:]:
-            if int(item[2])==0:
-                if cat!="d": offset +=2
-                if cat=="f": offset -=2
-                print('''<tr><td colspan=2 style="background-color:#d1e6df; padding-left: {}em"><img class="td-folder" src="../IMG/opened-folder.png"><h4 style="display:inline">{}</h4></td></tr>'''.format(offset+1,item[1]))
-                cat="d"
+            if int(item[2]) == 0:
+                if cat != "d": offset += 2
+                if cat == "f": offset -= 2
+                print(
+                    '''<tr><td colspan=2 style="background-color:#d1e6df; padding-left: {}em"><img class="td-folder" src="../IMG/opened-folder.png"><h4 style="display:inline">{}</h4></td></tr>'''.format(
+                        offset + 1, item[1]))
+                cat = "d"
             else:
-                print('''<tr><td style="padding-left: {}em"><img class="td-file" src="../IMG/file.png">{}</td><td align="right" style="padding-right: 1em; width: 7em;" title="{} байт">{}</td></tr>'''.format(offset+2,item[1],sepp(item[2]),calc(int(item[2]))))
-                cat="f"
+                print(
+                    '''<tr><td style="padding-left: {}em"><img class="td-file" src="../IMG/file.png">{}</td><td align="right" style="padding-right: 1em; width: 7em;" title="{} байт">{}</td></tr>'''.format(
+                        offset + 2, item[1], sepp(item[2]), calc(int(item[2]))))
+                cat = "f"
     print('''</table><br>''')
     print('</div></div>')
-    print('<td style="width: 15%; vertical-align:middle" align="right"><p class="info"><b>{}</b> {}</p></td>'.format(r[5][:10],r[5][11:]))
-    print('<td style="width: 7%;"><a href="magnet:?xt=urn:btih:{}"><img src="../IMG/download40.png" align="right" alt="Скачать" title="Скачать по magnet-ссылке"></a></td></tr></table></div>'.format(r[2]))
+    print('<td style="width: 15%; vertical-align:middle" align="right"><p class="info"><b>{}</b> {}</p></td>'.format(
+        r[5][:10], r[5][11:]))
+    print(
+        '<td style="width: 7%;"><a href="magnet:?xt=urn:btih:{}"><img src="../IMG/download40.png" align="right" alt="Скачать" title="Скачать по magnet-ссылке"></a></td></tr></table></div>'.format(
+            r[2]))
     print('<div class="layer3" style="height: 91vh">')
     print('<div class="layer5"><h2>{}</h2></div>'.format(r[3]))
 else:
--- start.py
+++ start.py
@@ -1,27 +1,28 @@
 #!/usr/bin/env python3
 # -*- coding: utf-8 -*-
 
-import os, sys
+import os
+import sys
 import webbrowser
 from http.server import HTTPServer, CGIHTTPRequestHandler
 
-def start_server():
 
-    webdir = '.' # каталог с файлами HTML и подкаталогом cgi­bin для сценариев
-    port   = 9200  # http://servername/ если 80, иначе http://servername:xxxx/
-    if len(sys.argv) > 1: webdir = sys.argv[1]    # аргументы командной строки
-    if len(sys.argv) > 2: port = int(sys.argv[2]) # иначе по умолчанию ., 80
+def start_server():
+    webdir = '.'  # каталог с файлами HTML и подкаталогом cgi­bin для сценариев
+    port = 9200  # http://servername/ если 80, иначе http://servername:xxxx/
+    if len(sys.argv) > 1: webdir = sys.argv[1]  # аргументы командной строки
+    if len(sys.argv) > 2: port = int(sys.argv[2])  # иначе по умолчанию ., 80
     print('webdir "%s", port %s' % (webdir, port))
-    os.chdir(webdir)        # перейти в корневой веб­каталог
-    srvraddr = ('', port)   # имя хоста, номер порта
+    os.chdir(webdir)  # перейти в корневой веб­каталог
+    srvraddr = ('', port)  # имя хоста, номер порта
     srvrobj = HTTPServer(srvraddr, CGIHTTPRequestHandler)
     try:
-        srvrobj.serve_forever() # обслуживать клиентов до завершения
+        srvrobj.serve_forever()  # обслуживать клиентов до завершения
     except KeyboardInterrupt:
         pass
-    srvrobj.server_close
+    srvrobj.server_close()
 
-if __name__=='__main__':
+
+if __name__ == '__main__':
     webbrowser.open_new_tab("http://localhost:9200")
     start_server()
-
